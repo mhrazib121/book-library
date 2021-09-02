f43a923ef9eaea381b3381d80ef88179999b1a17
mhrazib121 <mhrazib121@gmail.com>
2021-09-02 10:27:52 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,46 +1,61 @@
-const loadBook=()=>{
+const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
     // Error massage 
-    const errorMassageField = document.getElementById('error-massage');
+    const errorMassageField = document.getElementById('massage');
     errorMassageField.innerText = "";
-    if(searchText === ""){
-        
+    if (searchText === "") {
+
         const p = document.createElement('p');
         p.innerText = " Please input your book name.";
         errorMassageField.appendChild(p);
     }
-    else{
+    else {
         loadData(searchText);
     }
-    searchField.value= "";
+    searchField.value = "";
 }
 
-const loadData=(searchItem)=>{
+const loadData = (searchItem) => {
     const url = `https://openlibrary.org/search.json?q=${searchItem}`;
     fetch(url)
-    .then(res=> res.json())
-    .then( data => displaySearchValue(data.docs))
+        .then(res => res.json())
+        .then(data => resultFound(data))
 }
 
-const displaySearchValue = (books)=>{
+// Find Results 
+const resultFound = (results) => {
+    // console.log(results);
+    const totalresult = document.getElementById('massage');
+    if (results.numFound === 0) {
+        const div = document.createElement('div');
+        div.innerHTML = `<h3 class="text-danger">No result found (${results.numFound}) </h3>`;
+        totalresult.appendChild(div);
+    }
+    else {
+        const div = document.createElement('div');
+        div.innerHTML = `<h3>Total result found (${results.numFound}) </h3>`;
+        totalresult.appendChild(div);
+        displaySearchValue(results.docs);
+    }
+}
+
+const displaySearchValue = (books) => {
     console.log(books);
     const displayField = document.getElementById('display-result');
     displayField.innerText = "";
-    if (books.length === 0){
-        const div = document.createElement('div');
-        div.innerHTML = `<p class="text-danger text-center"> No result found </p>`;
-        displayField.appendChild(div);
-    }
-    else{
-        books.forEach(book=>{
+        books.forEach(book => {
             console.log(book)
-            const div = document.createElement('div');
+            if(book.cover_i === undefined){
+                
+            }
+            else{
+                const div = document.createElement('div');
             div.classList.add('col');
-            div.innerHTML=`
+            div.innerHTML = `
                 <div class="card">
-                    <img class='w-75' src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
+                    <img class='img-fluid' src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
                     <div class="card-body">
                       <h5 class="card-title">${book.title}</h5>
                       <p class="card-text text-danger"> Author Name: ${book.author_name}</p>
@@ -49,7 +64,6 @@ const displaySearchValue = (books)=>{
                 </div>
             `
             displayField.appendChild(div);
+            }
         })
-    }
-    
 }
